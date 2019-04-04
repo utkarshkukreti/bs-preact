d8d16fa732126f4fe8d472b7bcc3e714473722dc
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-04-04 07:18:11 +0530

    start Preact.Router.Parser implementation

--- src/Preact_Router.ml
+++ src/Preact_Router.ml
@@ -10,3 +10,47 @@ module Url = struct
     in
     { path }
 end
+
+module Parser = struct
+  type 'a state =
+    { url : Url.t
+    ; value : 'a
+    }
+
+  type ('a, 'b) t = Parser of ('a state -> 'b state list)
+
+  let root value = Parser (fun state -> [ { state with value } ])
+
+  let string =
+    Parser
+      (fun state ->
+        match state.url.path with
+        | first :: rest -> [ { url = { path = rest }; value = state.value first } ]
+        | [] -> [])
+
+  let s string =
+    Parser
+      (fun state ->
+        match state.url.path with
+        | first :: rest when first == string -> [ { state with url = { path = rest } } ]
+        | _ -> [])
+
+  let ( </> ) (Parser a) (Parser b) =
+    Parser (fun state -> Belt.List.map (a state) b |. Belt.List.flatten)
+
+  let map value (Parser p) =
+    Parser
+      (fun state ->
+        p { state with value }
+        |. Belt.List.map (fun state' -> { state' with value = state.value state'.value })
+        )
+
+  let parse (Parser p) url =
+    let state = { url; value = (fun x -> x) } in
+    let rec get = function
+      | { url = { path = [] }; value } :: _ -> Some value
+      | _ :: rest -> get rest
+      | _ -> None
+    in
+    get (p state)
+end
--- tests/Preact_Router_Test.ml
+++ tests/Preact_Router_Test.ml
@@ -14,4 +14,29 @@ let () =
               go "" { R.Url.path = [] };
               go "foo" { R.Url.path = [ "foo" ] };
               go "//foo/" { R.Url.path = [ "foo" ] };
-              go "//foo/bar//baz/" { R.Url.path = [ "foo"; "bar"; "baz" ] })))
+              go "//foo/bar//baz/" { R.Url.path = [ "foo"; "bar"; "baz" ] }));
+      describe "Parser" (fun () ->
+          describe "parse" (fun () ->
+              let open R.Parser in
+              let go string parser parsed =
+                test
+                  ("\"" ^ string ^ "\"")
+                  (fun () ->
+                    expect (R.Parser.parse parser (R.Url.fromString string))
+                    |> toEqual parsed)
+              in
+              go "" (root ()) (Some ());
+              go "foo" (root ()) None;
+              go "foo" (s "foo" |> map ()) (Some ());
+              go "foo" string (Some "foo");
+              go "foo/bar" (s "foo" </> s "bar" |> map ()) (Some ());
+              go "foo/bar" (s "foo" </> string) (Some "bar");
+              go "foo/bar" (string </> s "bar") (Some "foo");
+              go
+                "foo/bar"
+                (string </> string |> map (fun a b -> a, b))
+                (Some ("foo", "bar"));
+              go "foo/bar/baz" (s "foo" </> s "bar" |> map ()) None;
+              go "foo/bar/baz" (s "foo" </> string) None;
+              go "foo/bar/baz" (string </> s "bar") None;
+              go "foo/bar/baz" (string </> string |> map (fun a b -> a, b)) None)))
